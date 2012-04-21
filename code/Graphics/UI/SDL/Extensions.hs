c690280a011a8aeb00cf6751bdd50b8ede07c65f
Steve Donnelly <the.steve.donnelly@gmail.com>
2012-04-21 16:45:51 -0400

    slightly refactoring SDL.Extensions

--- code/Graphics/UI/SDL/Extensions.hs
+++ code/Graphics/UI/SDL/Extensions.hs
@@ -1,40 +1,37 @@
-
-module IO.SDL.Events where
+module Graphics.UI.SDL.Extensions where
 import Data.List as List
 import Data.Map as Map
 import Data.Set as Set
-import Graphics.UI.SDL.Events as SE
-import Graphics.UI.SDL.Keysym as Keysym
-import IO.Buttons as Buttons
+import Graphics.UI.SDL as SDL
 import Prelude.Extensions as PreludeExt
 
 
 isNoEvent = \event -> case event of
-    SE.NoEvent -> True
+    SDL.NoEvent -> True
     _ -> False
     
 isKeyDownEvent = \event -> case event of
-    SE.KeyDown _ -> True
+    SDL.KeyDown _ -> True
     _ -> False
 
 isKeyUpEvent = \event -> case event of
-    SE.KeyUp _ -> True
+    SDL.KeyUp _ -> True
     _ -> False
+
+isKeyEvent = \event -> ((||) (isKeyDownEvent event) (isKeyUpEvent event))
 
 isQuitEvent = \event -> case event of
-    SE.Quit -> True
+    SDL.Quit -> True
     _ -> False
 
-normalizeSDLKeyEvents = \key_events -> let
-    normalizeEvent = \event -> case event of
-        (KeyDown (Keysym key_id modifiers unicode)) -> (True, key_id)
-        (KeyUp (Keysym key_id modifiers unicode)) -> (False, key_id)
-    in (List.map normalizeEvent key_events)
+normalizeKeyEvent = \event -> case event of
+    (KeyDown (Keysym key_id modifiers unicode)) -> (True, key_id)
+    (KeyUp (Keysym key_id modifiers unicode)) -> (False, key_id)
 
-processSDLKeyEvents = \key_events -> (processButtonEvents (normalizeSDLKeyEvents key_events))
+normalizeKeyEvents = (List.map normalizeKeyEvent)
 
 takeEvents = do
-    event <- SE.pollEvent
+    event <- SDL.pollEvent
     let no_events = (isNoEvent event)
     remaining <- (ifElse no_events (return []) takeEvents)
     return (ifElse no_events [] ((:) event remaining))
