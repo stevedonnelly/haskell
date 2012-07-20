e7f7ed3c530ee37c51b84d47cc8fc00a7c886b83
Steve Donnelly <the.steve.donnelly@gmail.com>
2012-07-19 22:09:12 -0400

    slightly changing Tuple.Extensions api to be more consistent, using first/second instead of fst/snd

--- code/Data/Tuple/Extensions.hs
+++ code/Data/Tuple/Extensions.hs
@@ -5,32 +5,34 @@ import Data.Tuple as Tuple
 setSubelement :: (a -> b -> a) -> (a -> b) -> (b -> c -> b) -> (a -> c -> a)
 setSubelement = \setOuter getInner setInner tuple value -> (setOuter tuple (setInner (getInner tuple) value))
 
-setFst (a, b) x = (x, b)
-setSnd (a, b) x = (a, x)
+first2 (a, b) = a
+second2 (a, b) = b
+setFirst2 (a, b) x = (x, b)
+setSecond2 (a, b) x = (a, x)
 
-fst3 (a, b, c) = a
-snd3 (a, b, c) = b
+first3 (a, b, c) = a
+second3 (a, b, c) = b
 third3 (a, b, c) = c
-setFst3 (a, b, c) x = (x, b, c)
-setSnd3 (a, b, c) x = (a, x, c)
+setFirst3 (a, b, c) x = (x, b, c)
+setSecond3 (a, b, c) x = (a, x, c)
 setThird3 (a, b, c) x = (a, b, x)
 
-fst4 (a, b, c, d) = a
-snd4 (a, b, c, d) = b
+first4 (a, b, c, d) = a
+second4 (a, b, c, d) = b
 third4 (a, b, c, d) = c
 fourth4 (a, b, c, d) = d
-setFst4 (a, b, c, d) x = (x, b, c, d)
-setSnd4 (a, b, c, d) x = (a, x, c, d)
+setFirst4 (a, b, c, d) x = (x, b, c, d)
+setSecond4 (a, b, c, d) x = (a, x, c, d)
 setThird4 (a, b, c, d) x = (a, b, x, d)
 setFourth4 (a, b, c, d) x = (a, b, c, x)
 
-fst5 (a, b, c, d, e) = a
-snd5 (a, b, c, d, e) = b
+first5 (a, b, c, d, e) = a
+second5 (a, b, c, d, e) = b
 third5 (a, b, c, d, e) = c
 fourth5 (a, b, c, d, e) = d
 fifth5 (a, b, c, d, e) = e
-setFst5 (a, b, c, d, e) x = (x, b, c, d, e)
-setSnd5 (a, b, c, d, e) x = (a, x, c, d, e)
+setFirst5 (a, b, c, d, e) x = (x, b, c, d, e)
+setSecond5 (a, b, c, d, e) x = (a, x, c, d, e)
 setThird5 (a, b, c, d, e) x = (a, b, x, d, e)
 setFourth5 (a, b, c, d, e) x = (a, b, c, x, e)
 setFifth5 (a, b, c, d, e) x = (a, b, c, d, x)
