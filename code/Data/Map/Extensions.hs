cda2a932d932e2b2db581f1078e53866b906fbd1
Steve Donnelly <the.steve.donnelly@gmail.com>
2012-07-31 17:57:17 -0400

    added memoize function Data.Map.Extensions (uses map to lookup cached computations)

--- code/Data/Map/Extensions.hs
+++ code/Data/Map/Extensions.hs
@@ -5,6 +5,8 @@ import Data.Map as Map
 import Data.Maybe as Maybe
 import Prelude.Extensions as PreludeExt
 
+lookupIf :: Ord a => a -> (Map a b) -> (Bool, b)
+lookupIf = \key map -> (splitMaybe (Map.lookup key map)) 
 
 inverse :: Ord a => Ord b => (Map a b) -> (Map b [a])
 inverse = \map -> let
@@ -32,5 +34,13 @@ splitLessEqual :: Ord k => k -> (Map k a) -> (Map k a, Map k a)
 splitLessEqual = \key map -> let
     (less, equal, greater) = (Map.splitLookup key map)
     in (ifElse (isJust equal) (Map.insert key (fromJust equal) less) less, greater)
+
+memoize :: Ord a => (a -> b) -> (a -> Map a b -> (b, Map a b))
+memoize = \function -> let
+    memoized = \input cache -> let
+        (is_cached, cached) = (lookupIf input cache) 
+        output = (function input)
+        in (ifElse is_cached (cached, cache) (output, Map.insert input output cache))
+    in memoized
 
 
