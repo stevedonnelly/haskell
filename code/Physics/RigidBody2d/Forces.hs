a4bdf5af9c73238975fd3bd3af3fecd95eec4cfe
Steve Donnelly <the.steve.donnelly@gmail.com>
2012-08-29 02:11:53 -0400

    updating Physics.RigidBody2d.Forces to use partially qualified module names

--- code/Physics/RigidBody2d/Forces.hs
+++ code/Physics/RigidBody2d/Forces.hs
@@ -3,20 +3,19 @@ import Algebra.Vector as Vector
 import Data.List as List
 import Geometry.Vector2d as Vector2d
 import Physics.Dynamics as Dynamics
-import qualified Physics.Particle as Particle
 import Physics.RigidBody2d as RigidBody2d
 import Prelude.Extensions as PreludeExt
 
 linearForce = \rigid_body direction acceleration -> let
-    mass = (Physics.RigidBody2d.mass rigid_body)
-    position = (Physics.RigidBody2d.position rigid_body)
+    mass = (RigidBody2d.mass rigid_body)
+    position = (RigidBody2d.position rigid_body)
     in (position, Dynamics.directedForce mass (Vector2d.fromAngle direction) acceleration)
 
 relativeLinearForce = \rigid_body direction acceleration -> let
     in (linearForce rigid_body ((+) (orientation rigid_body) direction) acceleration)
 
 spinForces = \rigid_body acceleration -> let
-    position = (Physics.RigidBody2d.position rigid_body)
+    position = (RigidBody2d.position rigid_body)
     forward_axis = (Vector2d.fromAngle (orientation rigid_body))
     right_axis = (Vector2d.rotate forward_axis (toRational ((/) (Prelude.negate pi) 2)))
     forward_force = (directedForce (inertiaMoment rigid_body) forward_axis ((/) acceleration 2))
@@ -25,12 +24,12 @@ spinForces = \rigid_body acceleration -> let
     in [left_force, right_force]
 
 linearFrictionForce = \rigid_body acceleration time -> let
-    velocity = (Physics.RigidBody2d.velocity rigid_body)
+    velocity = (RigidBody2d.velocity rigid_body)
     decceleration = (Prelude.negate (Dynamics.frictionAcceleration (Vector.length velocity) acceleration time))
     in (linearForce rigid_body (Vector2d.toAngle velocity) decceleration)
 
 spinFrictionForces = \rigid_body acceleration time -> let
-    velocity = (Physics.RigidBody2d.angularVelocity rigid_body)
+    velocity = (RigidBody2d.angularVelocity rigid_body)
     decceleration = (Prelude.negate ((*) (signum velocity) (Dynamics.frictionAcceleration (abs velocity) acceleration time)))
     in (spinForces rigid_body decceleration)
 
@@ -40,15 +39,15 @@ frictionForces = \body friction angular_friction timestep -> let
     in ((:) forward_friction spin_friction)
 
 unlimitedLinearForces = \rigid_body max_velocity forces -> let
-    velocity = (Physics.RigidBody2d.velocity rigid_body)
+    velocity = (RigidBody2d.velocity rigid_body)
     withinMax = \(position, force) -> (isUnlimitedForce velocity max_velocity force)
     in (List.filter withinMax forces)
 
 unlimitedSpinForces = \rigid_body max_velocity forces -> let
-    velocity = (Physics.RigidBody2d.angularVelocity rigid_body)
+    velocity = (RigidBody2d.angularVelocity rigid_body)
     withinMax = \(position, force) -> let
         less_than_max = ((<) (abs velocity) max_velocity)
-        direction = (Vector.subtract position (Physics.RigidBody2d.position rigid_body))
+        direction = (Vector.subtract position (RigidBody2d.position rigid_body))
         is_opposite = ((/=) (signum velocity) (signum (crossProduct direction force)))
         in ((||) less_than_max is_opposite)
     in (List.filter withinMax forces)
