a509e21ad097dd82ad0bdbfe6bd8410bbbe8516f
Steve Donnelly <the.steve.donnelly@gmail.com>
2014-02-23 19:33:52 -0500

    removing IndexToken type, Token now stores a posititon

--- code/Text/Scanner.hs
+++ code/Text/Scanner.hs
@@ -10,22 +10,16 @@ import Prelude.Extensions as PreludeExt
 import Text.Regex.Posix
 
 
-type Token = (Int, String)
-tokenId = first2
-text = second2
-setText = setSecond2
-type IndexToken = (Int, Token)
-index = first2
-indexTokenId = ((.) tokenId token)
-indexTokenText = ((.) text token)
-token = second2
-setIndexTokenToken = setSecond2
-setIndexTokenText = \tok text -> (setIndexTokenToken tok (setText (token tok) text))
+type Token = (Int, Int, String)
+tokenType = first3
+tokenPosition = second3
+tokenText = third3
+setTokenText = setThird3
 
 matchFront :: String -> String -> (Bool, String)
 matchFront = \input regex -> (input =~ ("\\`" ++ regex), input =~ ("\\`" ++ regex))
 
-scan :: [String] -> String -> ([IndexToken], [(Int, Char)])
+scan :: [String] -> String -> ([Token], [(Int, Char)])
 scan = \patterns input -> let
     rules = (zipIndices0 patterns)
     selectMatch = \input previous rule -> let
@@ -33,15 +27,14 @@ scan = \patterns input -> let
         select_new = ((&&) is_match ((>) (length text) (length (snd previous))))
         in (ifElse select_new (fst rule, text) previous)
     scan = \input index -> let
-        token = (List.foldl (selectMatch input) (-1, "") rules)
-        match_length = (length (text token))
+        (match_type, match_text) = (List.foldl (selectMatch input) (-1, "") rules)
+        match_length = (length match_text)
         after_match = (drop match_length input)
         (remaining, next_index, tokens, errors) = (ifElse ((/=) match_length 0) 
-            (after_match, (+) index match_length, [(index, token)], []) 
+            (after_match, (+) index match_length, [(match_type, index, match_text)], []) 
             (tail input, (+) index 1, [], [(index, head input)]))
         (recurse_tokens, recurse_errors) = (scan remaining next_index)
         in (ifElse (List.null input) ([], []) ((++) tokens recurse_tokens, (++) errors recurse_errors))
     in (scan input 0)
-
 
 
