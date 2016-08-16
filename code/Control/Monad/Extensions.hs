afc8165b877dff189754943689b126bbd1a711ca
Steve Donnelly <the.steve.donnelly@gmail.com>
2016-08-15 22:43:31 -0400

    also adding whileM transformer

--- code/Control/Monad/Extensions.hs
+++ code/Control/Monad/Extensions.hs
@@ -1,4 +1,5 @@
 module Control.Monad.Extensions where
+import Control.Applicative
 import Prelude.Extensions as PreludeExt
 
 untilM :: Monad m => (a -> m Bool) -> (a -> m a) -> a -> m a 
@@ -9,4 +10,5 @@ untilM = \condition transform initial -> do
         (untilM condition transform next)}
     (ifElse stop (return initial) recurse)
 
-
+whileM :: Monad m => Applicative m => (a -> m Bool) -> (a -> m a) -> a -> m a 
+whileM = \condition -> (untilM ((.) (liftA not) condition))
