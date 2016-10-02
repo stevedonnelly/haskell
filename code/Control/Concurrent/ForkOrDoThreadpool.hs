cfdc404ef96961e8c584472597c6378a552bbb23
Steve Donnelly <the.steve.donnelly@gmail.com>
2016-10-02 10:59:19 -0400

    renaming Threadpool to ForkOrDo pool (since theres backing queue); removing unneded inner MVar

--- code/Control/Concurrent/ForkOrDoThreadpool.hs
+++ code/Control/Concurrent/ForkOrDoThreadpool.hs
@@ -1,39 +1,39 @@
-module Control.Concurrent.Threadpool where
+module Control.Concurrent.ForkOrDoPool where
 import Control.Concurrent
 import Control.Concurrent.MVar as MVar
 import Data.List as List
 import Data.Maybe as Maybe
 import Prelude.Extensions as PreludeExt
 
-type Threadpool = [(ThreadId, MVar (MVar (IO ())))]
+type ForkOrDoPool = [(ThreadId, MVar (IO ()))]
 
-createThreadpool :: Int -> IO Threadpool
-createThreadpool = \number_of_threads -> do
+createPool :: Int -> IO ForkOrDoPool
+createPool = \number_of_threads -> do
     let work_lock = MVar.newEmptyMVar
     work_locks <- (mapM id (replicate number_of_threads work_lock))
     thread_ids <- (mapM ((.) forkIO workerThread) work_locks)
     (return (zip thread_ids work_locks))
 
-workerThread :: (MVar (MVar (IO ()))) -> IO ()
+workerThread :: (MVar (IO ())) -> IO ()
 workerThread = \work_lock -> do
-    work_holder <- MVar.newEmptyMVar
-    (MVar.putMVar work_lock work_holder)
-    work <- (MVar.takeMVar work_holder)
+    work <- (MVar.readMVar work_lock)
     work
+    (MVar.takeMVar work_lock)
     (workerThread work_lock)
 
-submitTask :: Threadpool -> IO () -> IO Bool
+submitTask :: ForkOrDoPool -> IO () -> IO Bool
 submitTask = \threadpool task -> do
     let {submit_first = do
         let (thread_id, work_lock) = (List.head threadpool)
-        work_holder <- (MVar.tryTakeMVar work_lock)
-        let {available = do
-            (MVar.putMVar (fromJust work_holder) task)
-            (return True)}
-        (ifElse (isJust work_holder) available (return False))}
+        (MVar.tryPutMVar work_lock task)}
     let {try_submit = do
         success <- submit_first
         (ifElse success (return True) (submitTask (List.tail threadpool) task))} 
     (ifElse (List.null threadpool) (return False) try_submit)
+
+forkOrDo :: ForkOrDoPool -> IO () -> IO ()
+forkOrDo = \threadpool task -> do
+    success <- (submitTask threadpool task)
+    (doIf (not success) task)
 
 
