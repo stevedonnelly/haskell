e9839d207e577e5c8ec294d977fd13f9f486ba3c
Steve Donnelly <the.steve.donnelly@gmail.com>
2016-10-05 22:55:42 -0400

    adding wrapper for creating and connecting client socket, retries until successful

--- code/Network/Socket/Extensions.hs
+++ code/Network/Socket/Extensions.hs
@@ -1,4 +1,7 @@
 module Network.Socket.Extensions where
+import Control.Concurrent
+import Control.Exception
+import Data.List as List
 import Network.Socket
 
 extended_hints = defaultHints {addrFlags = [AI_ADDRCONFIG], addrSocketType = Stream}
@@ -18,6 +21,11 @@ lookupHostPort = \host port -> (getAddrInfo (Just extended_hints) (Just host) (J
 lookupHost :: String -> IO [AddrInfo]
 lookupHost = \host -> (getAddrInfo (Just extended_hints) (Just host) Nothing)
 
+resolveHostPort :: String -> String -> IO AddrInfo
+resolveHostPort = \host port -> do
+    addresses <- (lookupHostPort host port)
+    (return (List.head addresses))
+
 listeningSocket :: Integral p => p -> IO Socket
 listeningSocket = \port -> do
     socket <- inetSocket
@@ -25,4 +33,19 @@ listeningSocket = \port -> do
     (listen socket maxListenQueue)
     (return socket)
     
+clientSocket :: String -> String -> IO Socket
+clientSocket = \host port -> do
+    address <- (resolveHostPort host port)
+    socket <- inetSocket
+    let {handleConnectFailure = \exception -> do 
+        (close socket)
+        (threadDelay 3000000)
+        (clientSocket host port)}
+    let handleConnectFailureTyped = handleConnectFailure :: SomeException -> IO Socket
+    let {doConnect = do 
+        (connect socket (addrAddress address))
+        (return socket)}
+    (catch doConnect handleConnectFailureTyped)
+
+
 
