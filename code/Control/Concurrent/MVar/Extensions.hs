fa02fd0cd99c8f0dfdf95f53342b3799dc5a8685
Steve Donnelly <the.steve.donnelly@gmail.com>
2016-10-22 09:57:43 -0400

    adding writeMVar function

--- code/Control/Concurrent/MVar/Extensions.hs
+++ code/Control/Concurrent/MVar/Extensions.hs
@@ -3,9 +3,15 @@ import Control.Concurrent.MVar as MVar
 import Prelude.Extensions as PreludeExt
 
 
+writeMVar :: MVar a -> a -> IO ()
+writeMVar = \mvar value -> do 
+    (MVar.tryTakeMVar mvar)
+    success <- (MVar.tryPutMVar mvar value)
+    let try_again = (writeMVar mvar value)
+    (doIf (not success) try_again)
+
 transformMVar :: MVar a -> (a -> a) -> IO (a, a)
 transformMVar = \mvar transform -> do
     let wrap = \x -> let y = (transform x) in (y, (x, y))
     (modifyMVar mvar ((.) return wrap))
-
 
