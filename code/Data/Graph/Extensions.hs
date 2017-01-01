a312de29527054b7a937e70c35e71f81d6028a4d
Steve Donnelly <the.steve.donnelly@gmail.com>
2017-01-01 16:02:23 -0500

    adding first draft of A* graph search

--- code/Data/Graph/Extensions.hs
+++ code/Data/Graph/Extensions.hs
@@ -3,6 +3,7 @@ module Data.Graph.Extensions where
 import Data.List as List
 import Data.Map as Map
 import Data.Map.Extensions as MapExt
+import Data.Maybe as Maybe 
 import Data.Set as Set
 import Data.Tuple as Tuple
 import Prelude.Extensions as PreludeExt
@@ -58,5 +59,30 @@ connectedComponents = \graph -> let
 
 union :: Ord vertex => (Graph vertex) -> (Graph vertex) -> (Graph vertex)
 union = \graph0 graph1 -> (Map.unionWith (++) graph0 graph1)
+
+
+shortestPathWithHeuristic :: Ord node => Real cost => Show (node) => Show cost => node -> (node -> [node]) -> (node -> node -> cost) -> (node -> cost) -> (Map node cost -> Bool) -> (Map node cost, Map node node)
+shortestPathWithHeuristic = \start_node neighbors cost heuristic completion -> let
+    queueNeighbor = \node node_cost best (queue, queue_lookup) neighbor -> let
+        neighbor_estimate = (sum [node_cost, cost node neighbor, heuristic neighbor])
+        previous_estimate = (Map.lookup neighbor queue_lookup)
+        updated_case = (Set.insert (neighbor_estimate, node, neighbor) queue, Map.insert neighbor neighbor_estimate queue_lookup)
+        in (ifElse ((&&) (Map.notMember neighbor best)
+                ((||) (Maybe.isNothing previous_estimate) ((<) neighbor_estimate (Maybe.fromJust previous_estimate))))
+            updated_case
+            (queue, queue_lookup))
+    next = \(queue, queue_lookup, best, path) -> let
+        ((estimated_cost, previous, node), remaining) = (Set.deleteFindMin queue)
+        remaining_lookup = (Map.delete node queue_lookup)
+        actual_cost = ((+) ((!) best previous) (cost previous node))
+        updated_best = (Map.insert node actual_cost best)
+        updated_path = (Map.insert node previous path)
+        (updated_queue, updated_lookup) = (List.foldl (queueNeighbor node actual_cost updated_best) (remaining, remaining_lookup) (neighbors node))
+        in (updated_queue, updated_lookup, updated_best, updated_path)
+    condition = \(queue, queue_lookup, best, path) -> (completion best)
+    (queue, queue_lookup) = (List.foldl (queueNeighbor start_node 0 Map.empty) (Set.empty, Map.empty) (neighbors start_node))
+    initial = (queue, queue_lookup, Map.singleton start_node 0, Map.empty)
+    (_, _, best, path) = (until condition next initial)
+    in (best, path)
 
 
